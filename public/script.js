17ac4647b6d83f259cf5f97caa5a32652d467005
Justin Beckwith <justin.beckwith@gmail.com>
2016-02-21 13:46:38 -0800

    add snackbar

--- public/script.js
+++ public/script.js
@@ -2,6 +2,7 @@
 var cnt = document.getElementById('cnt');
 var btn = document.getElementById('btn');
 var progress = document.getElementById('progress');
+var snackbar = document.getElementById('snackbar');
 
 progress.addEventListener('mdl-componentupgraded', function() {
     this.MaterialProgress.setProgress(clicks);
@@ -19,5 +20,11 @@ nub.subscribe({
 });
 
 btn.onclick = function() {
-    fetch('/click', { method: 'post' });
+    fetch('/click', { method: 'post' })
+        .then(function(response) {
+            snackbar.MaterialSnackbar.showSnackbar({
+                message: 'Thanks!',
+                timeout: 2000
+            });
+        });
 }
