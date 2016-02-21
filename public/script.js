86f700682fd7f29a81cf1190452f5835ede56d9c
Justin Beckwith <justin.beckwith@gmail.com>
2016-02-21 13:21:42 -0800

    use material design lite

--- public/script.js
+++ public/script.js
@@ -1,6 +1,11 @@
 
 var cnt = document.getElementById('cnt');
 var btn = document.getElementById('btn');
+var progress = document.getElementById('progress');
+
+progress.addEventListener('mdl-componentupgraded', function() {
+    this.MaterialProgress.setProgress(clicks);
+});
 
 var nub = PUBNUB.init({
     subscribe_key: subscribeKey
@@ -9,9 +14,7 @@ var nub = PUBNUB.init({
 nub.subscribe({
     channel: 'click',
     message: function(m){
-        console.log(m);
-        cnt.innerText = m;
-        cnt.style.width = m + "%";
+        progress.MaterialProgress.setProgress(m);
     }
 });
 
